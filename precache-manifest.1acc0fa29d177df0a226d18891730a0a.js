0f539bd723d05f88a1ffcea25b0c020b0e381a03
Huzaifa Jameel <huzaifajameel2@gmail.com>
2020-09-29 22:47:00 +0500

    Updates

---
+++ precache-manifest.1acc0fa29d177df0a226d18891730a0a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9914825b2519d651e8dacb91380de5c4",
+    "url": "/Todo-app/index.html"
+  },
+  {
+    "revision": "ba1f4136c278b40bf14f",
+    "url": "/Todo-app/static/css/main.295221e8.chunk.css"
+  },
+  {
+    "revision": "b646bfa45a1ddd91761e",
+    "url": "/Todo-app/static/js/2.a74d1753.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Todo-app/static/js/2.a74d1753.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba1f4136c278b40bf14f",
+    "url": "/Todo-app/static/js/main.dff16326.chunk.js"
+  },
+  {
+    "revision": "1eb6d6ec195e648776ea",
+    "url": "/Todo-app/static/js/runtime-main.375335ae.js"
+  }
+]);
